d94c67ae6c11afe0eb3f0226a38522b0c2d70576
Kevin.M <kema2501@student.miun.se>
2026-05-19 10:15:59 +0200

    Lägg till 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,17 @@ import { Contact } from './pages/contact/contact';
 import { Booking } from './pages/booking/booking';
 import { Login } from './pages/login/login';
 import { Admin } from './pages/admin/admin';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
-    { path: "", component: Homepage },
+    { path: "homepage", component: Homepage},
     { path: "menu", component: Menu },
     { path: "about", component: About },
     { path: "contact", component: Contact },
     { path: "booking", component: Booking },
     { path: "login", component: Login },
-    { path: "admin", component: Admin }
+    { path: "admin", component: Admin },
+    { path: "", redirectTo: "homepage", pathMatch: "full" },
+    { path: "404", component: NotFound },
+    { path: "**", redirectTo: "404", pathMatch: "full" }
 ];
