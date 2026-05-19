d0d90d145e2d2146423c3bf918e6514546b9ff81
Kevin.M <kema2501@student.miun.se>
2026-05-19 13:27:24 +0200

    Lägg till utseende för header ink mobilmeny

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+    // Is menu open?
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+}
