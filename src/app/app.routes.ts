f27d4dd2beb5d55d811779d0e5b8590f4206d5ca
Kevin.M <kema2501@student.miun.se>
2026-05-19 10:08:21 +0200

    Lägg till komponenter och interfaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './pages/homepage/homepage';
+import { Menu } from './pages/menu/menu';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Booking } from './pages/booking/booking';
+import { Login } from './pages/login/login';
+import { Admin } from './pages/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Homepage },
+    { path: "menu", component: Menu },
+    { path: "about", component: About },
+    { path: "contact", component: Contact },
+    { path: "booking", component: Booking },
+    { path: "login", component: Login },
+    { path: "admin", component: Admin }
+];
